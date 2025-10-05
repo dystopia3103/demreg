31461cbf8c41f06fd36ba92f227ca866b3de4762
nku1313 <nku121181@gmail.com>
2025-10-05 16:18:32 +0200

    optimize code with numba / JIT (not parallel & without fastmath)

--- python/dem_reg_map.py
+++ python/dem_reg_map.py
@@ -1,6 +1,8 @@
 import numpy as np
+from numba import njit
 
-def dem_reg_map(sigmaa,sigmab,U,W,data,err,reg_tweak,nmu=500):
+@njit  # No parallel or fastmath
+def dem_reg_map(sigmaa, sigmab, U, W, data, err, reg_tweak, nmu=500):
     """
     dem_reg_map
     computes the regularization parameter
@@ -28,34 +30,41 @@ def dem_reg_map(sigmaa,sigmab,U,W,data,err,reg_tweak,nmu=500):
         regularization paramater
 
     """
- 
 
-    nf=data.shape[0]
-    nreg=sigmaa.shape[0]
+    nf = data.shape[0]
+    nreg = sigmaa.shape[0]
 
-    arg=np.zeros([nreg,nmu])
-    discr=np.zeros([nmu])
+    arg = np.zeros((nreg, nmu))
+    discr = np.zeros(nmu)
 
-    sigs=sigmaa[:nf]/sigmab[:nf]
-    maxx=max(sigs)
-    # minx=min(sigs)**2.0*1E-2
-    ## Useful to make the lower limit smaller?
-    minx=min(sigs)**2.0*1E-4
+    sigs = sigmaa[:nf] / sigmab[:nf]
+    maxx = np.max(sigs)
+    minx = np.min(sigs)**2.0 * 1e-4
 
-    ## Range from original non-map code
-    # maxx=max(sigs)*1E3
-    # minx=max(sigs)*1E-15
+    step = (np.log(maxx) - np.log(minx)) / (nmu - 1.0)
+    mu = np.exp(np.arange(nmu) * step) * minx
 
-    step=(np.log(maxx)-np.log(minx))/(nmu-1.)
-    mu=np.exp(np.arange(nmu)*step)*minx
-    for kk in np.arange(nf):
-        coef=data@U[kk,:]
-        for ii in np.arange(nmu):
-            arg[kk,ii]=(mu[ii]*sigmab[kk]**2*coef/(sigmaa[kk]**2+mu[ii]*sigmab[kk]**2))**2
-    
-    discr=np.sum(arg,axis=0)-np.sum(err**2)*reg_tweak
-  
-    opt=mu[np.argmin(np.abs(discr))]
-    # print(opt)
+    for kk in range(nf):
+        coef = 0.0
+        for jj in range(U.shape[1]):
+            coef += data[jj] * U[kk, jj]
+        for ii in range(nmu):
+            val = mu[ii] * sigmab[kk]**2 * coef / (sigmaa[kk]**2 + mu[ii] * sigmab[kk]**2)
+            arg[kk, ii] = val * val
 
-    return opt
+    # Sum over first axis manually (Numba prefers explicit loops)
+    for ii in range(nmu):
+        s = 0.0
+        for kk in range(nreg):
+            s += arg[kk, ii]
+        discr[ii] = s - np.sum(err**2) * reg_tweak
+
+    opt_index = 0
+    min_abs_discr = abs(discr[0])
+    for ii in range(1, nmu):
+        abs_val = abs(discr[ii])
+        if abs_val < min_abs_discr:
+            min_abs_discr = abs_val
+            opt_index = ii
+
+    return mu[opt_index]
